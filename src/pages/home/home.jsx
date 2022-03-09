efbd2920f94bd937660caf7dcecd22fcfb4bf225
ivsp <ivsp93@gmail.com>
2022-03-10 00:24:04 +0100

    Iconos en movimiento y fetch

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from "react";
-import "../../boostrap-icons/font/weathericons-regular-webfont.svg"
-import "../../boostrap-icons/font/weathericons-regular-webfont.eot"
-import "../../boostrap-icons/font/weathericons-regular-webfont.ttf"
-import "../../boostrap-icons/font/weathericons-regular-webfont.woff"
-import "../../boostrap-icons/font/weathericons-regular-webfont.woff2"
-import "../../boostrap-icons/css/weather-icons-wind.css"
-import "../../boostrap-icons/css/weather-icons-wind.min.css"
-import "../../boostrap-icons/css/weather-icons.css"
-import "../../boostrap-icons/css/weather-icons.min.css"       
+import { ReactComponent as Cloudy } from "../../weather-icons/cloudy.svg";
+import { ReactComponent as Rainy } from "../../weather-icons/rainy.svg";
+import { ReactComponent as Cloudysun } from "../../weather-icons/cloudy.sun.svg";
+import { ReactComponent as Cloudymoon } from "../../weather-icons/cloudymoon.svg";
+import { ReactComponent as Cloudylighting } from "../../weather-icons/cloudy.lightning.svg";
+import { ReactComponent as Cloudyrainlighting } from "../../weather-icons/cloudy.rain.lightning.svg";
+import { ReactComponent as Sunny } from "../../weather-icons/sunny.svg";
+import { ReactComponent as Clearnight } from "../../weather-icons/clearnight.svg";
+import { ReactComponent as Sunnywind } from "../../weather-icons/sunny.wind.svg";
+import { ReactComponent as Snowy } from "../../weather-icons/snowy.svg";
+
 import "./index.css";
-                           
-
-
 
 function Home() {
   const KEY = "6ec1b7595153b67cc7506c3c5b5e8f64";
@@ -20,39 +19,91 @@ function Home() {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
   const [weatherData, setWeatherData] = useState([]);
-  const [urlImage, setUrlImage] = useState("");
+  const [urlImages, setUrlImages] = useState([]);
+  const [keyWords, setKeyWords] = useState(["sunset", "portrait", "macro"]);
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${CITY}&limit=1&appid=${KEY}`
-    )
-      .then((r) => r.json())
-      .then((location) => {
-        console.log(location);
-        setLatitude(location[0].lat);
-        setLongitude(location[0].lon);
-        console.log(location[0].name);
-        fetch(
-          `http://api.openweathermap.org/data/2.5/weather?lat=${location[0].lat}&lon=${location[0].lon}&appid=${KEY}`
-        )
-          .then((r) => r.json())
-          .then((data) => {
-            console.log(data);
-            setWeatherData(data);
-          });
-      });
+    // fetch(
+    //   `http://api.openweathermap.org/geo/1.0/direct?q=${CITY}&limit=1&appid=${KEY}`
+    // )
+    //   .then((r) => r.json())
+    //   .then((location) => {
+    //     setLatitude(location[0].lat);
+    //     setLongitude(location[0].lon);
+    //     fetch(
+    //       `http://api.openweathermap.org/data/2.5/weather?lat=${location[0].lat}&lon=${location[0].lon}&appid=${KEY}`
+    //     )
+    //       .then((r) => r.json())
+    //       .then((data) => {
+    //         setWeatherData(data);
+    //       });
+    //   });
 
-    fetch("https://source.unsplash.com/category/nature/?sunset") //fetch para obtener la imagen
-      .then((r) => {
-        console.log(r)
-        setUrlImage(r.url);
-      });
+    fetch(
+      `https://source.unsplash.com/category/nature/800x600/?${keyWords[0]},${keyWords[1]},${keyWords[2]}`
+    ).then((r) => {
+      console.log(r);
+      urlImages.push(r.url);
+      setUrlImages([...urlImages]);
+      console.log(urlImages);
+    });
   }, [CITY]);
+
+  function onNext() {
+    console.log("dentro del onNext");
+    console.log(urlImages);
+
+    // console.log(urlImage);
+    console.log(counter);
+    if (counter < 4) {
+      if (urlImages.length < 4) {
+        fetch(
+          `https://source.unsplash.com/category/nature/800x600/?${keyWords[0]},${keyWords[1]},${keyWords[2]}`
+        ) //fetch para obtener la imagen
+          .then((r) => {
+            console.log(r);
+            urlImages.push(r.url);
+            setUrlImages([...urlImages]);
+          });
+      }
+      setCounter(counter + 1);
+    } else {
+      setCounter(0);
+    }
+  }
+
+  const onPrevious = () => {
+    if (counter !== 0) {
+      setCounter(counter - 1);
+      console.log(counter);
+      // console.log(urlImage);
+    }
+  };
 
   return (
     <React.Fragment>
-      {urlImage !== "" ? <img className="image" src={urlImage} alt="" /> : ""}
-      <i className="wi wi-day-sunny" ></i>
+      <section className="container">
+        <button onClick={onPrevious}>PREV</button>
+
+        {urlImages[0] !== "" ? (
+          <img className="image" src={urlImages[counter]} alt="" />
+        ) : (
+          ""
+        )}
+        {counter === 3 ? "" : <button onClick={onNext}>NEXT</button>}
+        <i className="wi wi-day-sunny"></i>
+        {/* <Cloudy></Cloudy>
+        <Rainy></Rainy>
+        <Cloudysun></Cloudysun>
+        <Cloudylighting></Cloudylighting>
+        <Cloudymoon></Cloudymoon>
+        <Cloudyrainlighting></Cloudyrainlighting>
+        <Sunny></Sunny>
+        <Clearnight></Clearnight>
+        <Sunnywind></Sunnywind>
+        <Snowy></Snowy> */}
+      </section>
     </React.Fragment>
   );
 }
