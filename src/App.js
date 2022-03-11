70ff05fc5a465002f4cad801d58db504b92d8f78
Ivánsp <85313333+ivsp@users.noreply.github.com>
2022-03-11 09:19:21 +0100

    Revert "Feature/card component"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import Home from "./pages/home/home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <Home></Home>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
